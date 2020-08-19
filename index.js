26852d6ded0aff26d65c4ec0f92c2b2d77a9a9f7
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-08-19 12:25:03 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,59 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(info){
+    return {
+        firstName: info[0],
+        familyName: info[1],
+        title: info[2],
+        payPerHour: info[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords (empArr) {
+    return empArr.map(info => createEmployeeRecord(info))
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this 
+}
+
+function hoursWorkedOnDate(dateStamp){
+    let hoursIn = this.timeInEvents.find(function(event){
+        return event.date === dateStamp
+    })
+
+    let hoursOut = this.timeOutEvents.find(function(event){
+        return event.date === dateStamp
+    })
+
+    return (hoursOut.hour - hoursIn.hour) / 100
+}
+
+function wagesEarnedOnDate(dateStamp){
+    let pay = hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour
+    return pay
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,4 +72,15 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(empObjs){
+    //use wages earned on date
+    return empObjs.reduce(function(total, record){
+        return total + allWagesFor.call(record)
+    }.bind(this), 0)
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(record => record.firstName === firstName)
 }
