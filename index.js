fe1e810fd10158586c7d7d04cf23fa28160e7557
Steve Anderson <prototype909@msn.com>
2020-08-21 18:51:23 +0000

    Done.

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ let allWagesFor = function () {
 
     let payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d);
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    }.bind(this), 0)
 
     return payable;
 };
